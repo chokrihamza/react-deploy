a50e04d2137a8c0192bf9af0d394f1530d732466
chokrihamza <chokrihamza1994@gmail.com>
2020-10-09 23:38:18 +0200

    Updates

---
+++ precache-manifest.7434ad80ef06c037d227e6a510627a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ce620a1511c49bcacac03267b63e62",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "c589d870f4ec577fbc88",
+    "url": "/react-deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c9f1b629a75b60146e64",
+    "url": "/react-deploy/static/css/main.e97a7165.chunk.css"
+  },
+  {
+    "revision": "c589d870f4ec577fbc88",
+    "url": "/react-deploy/static/js/2.f267df44.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-deploy/static/js/2.f267df44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f1b629a75b60146e64",
+    "url": "/react-deploy/static/js/main.98f611b7.chunk.js"
+  },
+  {
+    "revision": "bad6914a9c194050fe64",
+    "url": "/react-deploy/static/js/runtime-main.e111f431.js"
+  }
+]);
